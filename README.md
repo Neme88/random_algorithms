df56715b90f0682f4eb1acfcdaf584976daa7b0d
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-02-19 20:50:04 +0200

    feat: add buble_sort implementation

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # random_algorithms
-this repo is dedicated soely for random algorithms implementation in noo particular order think of it as rough idea pan if you will. 
+This repo is dedicated for the sole purpose of random algorithms implementation in no particular order, think of it as rough idea pan if you will. 
---
+++ bubble_sort.py
@@ -0,0 +1,13 @@
+
+def bubble_sort(arr):
+    """Bubble Sort Algorithm"""
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]  # Swap
+                swapped = True
+        if not swapped:
+            break  # Optimization: Stop if no swaps occurred in this pass
+    return arr

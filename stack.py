eef8fe964a9b922e7225d5d4766538cabe739a53
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-02-21 22:45:51 +0200

    feat: add stack implementation

---
+++ stack.py
@@ -0,0 +1,13 @@
+stack = []
+
+# Pushing elements
+stack.append(1)
+stack.append(2)
+stack.append(3)
+
+# Popping elements
+print("Popped element:", stack.pop())
+print("Current Stack:", stack)
+
+# Peeking the top element
+print("Top element:", stack[-1])

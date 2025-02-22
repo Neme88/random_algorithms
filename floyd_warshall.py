7b94e82d527e23d412524ef8249344ffb4c8455b
Neme Nwaka <ccnwaka1988@gmail.com>
2025-02-22 21:47:11 +0200

    feat: add the core floyd warshall algorithm

---
+++ floyd_warshall.py
@@ -0,0 +1,9 @@
+# Floyd-Warshall Algorithm (All-Pairs Shortest Path)
+def floyd_warshall(graph):
+    n = len(graph)
+    dist = [[graph[i][j] for j in range(n)] for i in range(n)]
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+    return dist

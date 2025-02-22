a27f41e69fc5843d2cd7aa56dc0a7d066de86eb9
Neme Nwaka <ccnwaka1988@gmail.com>
2025-02-22 22:04:38 +0200

    Test: add test case foor the factoria algorithm

--- factoria_time.py
+++ factoria_time.py
@@ -4,3 +4,8 @@ from itertools import permutations
 
 def generate_permutations(arr):
     return list(permutations(arr))
+
+# Example Usage
+if __name__ == "__main__":
+     # Test O(N!)
+    print(generate_permutations([1, 2, 3]))  # Output: List of all permutations
